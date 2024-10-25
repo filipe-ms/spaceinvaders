c676d550433b631819a8eba8b708ff739e3f87d2
filipe-ms <lfms@cesar.school>
2024-10-25 01:08:19 -0300

    first commit

---
+++ build/build_files/obj/x64/Debug/Space/main.d
@@ -0,0 +1,8 @@
+obj/x64/Debug/Space/main.o: ../../src/main.c /usr/include/stdc-predef.h \
+ ../external/raylib-master/src/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h
+/usr/include/stdc-predef.h:
+../external/raylib-master/src/raylib.h:
+/usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h:
+/usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h:
